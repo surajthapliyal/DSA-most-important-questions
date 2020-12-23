1fc42703863abcb8c4ee1563a4b4401b1b28d3d7
Suraj <surajthapliyal18@gmail.com>
2020-12-23 20:27:29 +0530

    added more question

---
+++ RecursionAndBacktracking/backtracking/partitionAnArrayIntoTwoEqualSumArrays.java
@@ -0,0 +1,40 @@
+package backtracking;
+
+import java.util.ArrayList;
+
+public class partitionAnArrayIntoTwoEqualSumArrays {
+    public static void main(String[] args) {
+        int a[] = {2,1,2,3,4,8};
+        int sum = 0;
+        for(int item : a){
+            sum += item;
+        }
+        System.out.println("Sum = " + sum);
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean isPossible = sum%2==0 && partition(a, sum/2, 0, ans);          //or (sum&1)==0; to check even
+        
+        if(isPossible)
+        for(int item : ans) System.out.print(item + " ");
+
+        else System.out.println("Not possible!!");
+
+    }
+    static boolean partition(int a[], int sum, int i, ArrayList<Integer> ans){
+
+        // if i reaches the length of the array : (base condition)
+        // or if the sum given is less than 0:
+        if(i>=a.length || sum < 0) return false;
+        if(sum==0) return true;
+        
+        ans.add(a[i]);
+
+        boolean leftPossible = partition(a, sum-a[i], ++i, ans);
+        if(leftPossible) return true;
+
+        // if left side is not possible then remove the element which is added in the list:(backtrack)
+        ans.remove(ans.size()-1);
+        // now check for the right side :
+        return partition(a, sum, ++i, ans);
+    }
+}
